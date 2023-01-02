87dbb3912650b6af4d436119633038b3fe0ed2ff
sniffjoke <41764936+sniffjoke@users.noreply.github.com>
2023-01-03 00:22:20 +0100

    add project, learn types of props

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Card, {CardVariant} from "./components/Card";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Card width={'200px'} height={'200px'} variant={CardVariant.outlined} onClick={(num) => console.log('click', num)}>
+            <button>Кнопка</button>
+            <div>kdfjalkfjas</div>
+        </Card>
     </div>
   );
 }
---
+++ src/components/Card.tsx
@@ -0,0 +1,40 @@
+import React, {FC, ReactNode, useState} from 'react';
+
+export enum CardVariant {
+    outlined = 'outlined',
+    primary = 'primary'
+}
+
+interface CardProps {
+    width?: string
+    height?: string
+    children?: ReactNode
+    variant: CardVariant
+    onClick: (num: number) => void
+}
+
+const Card: FC<CardProps> = ({
+                                 width,
+                                 height,
+                                 variant,
+                                 onClick,
+                                 children,
+                             }) => {
+    const [state, setState] = useState(0)
+
+    return (
+        <div
+            style={{
+                width,
+                border: variant === CardVariant.outlined ? '1px solid gray' : 'none',
+                height,
+                background: variant === CardVariant.primary ? 'lightgray' : ''
+            }}
+            onClick={() => onClick(state)}
+        >
+            {children}
+        </div>
+    );
+};
+
+export default Card;
